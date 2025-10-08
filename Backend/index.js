753923b807d645eb2cc56419d0b36ad038659ad8
shivam kumar <shivamsharma27107@gmail.com>
2025-10-09 00:26:06 +0530

    header issue

--- Backend/index.js
+++ Backend/index.js
@@ -9,26 +9,19 @@ const feturesroutes = require('./routes/feturesroutes');
 const streamTokenRoutes = require('./routes/stream-token');
 const authMiddleware = require('./middlewares/authMiddleware');
 
-
 dotenv.config();
 connectToDb();
 
 const corsOptions = {
    origin: ['https://visionclassroo0m-jkiw.vercel.app', 'https://vision-classroom2.onrender.com'],
-<<<<<<< HEAD
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
    credentials: true,
-=======
->>>>>>> e8e53ce76d3f40c57b9775acc132b33f4f63f217
    optionsSuccessStatus: 200
 };
 
-
-
 const app = express();
 const PORT = process.env.PORT || 4000;
-
 
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -41,8 +34,6 @@ app.use('/api/stream-token', streamTokenRoutes);
 app.get("/", (req, res) => {
   res.send("Vision Classroom")
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
