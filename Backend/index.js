109e9a111d936e1376aa3c7cdbfa91585f6cfce0
Shivam kumar <shivamsharma27107@gmail.com>
2025-10-10 19:15:31 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 connectToDb();
 
 const corsOptions = {
-   origin: ['https://visionclassro0m.vercel.app', 'https://visionclassr0om.vercel.app', 'https://vision-classroom2.onrender.com'],
+   origin: ['https://visionclassro0m.vercel.app', 'https://visionclassr0om.vercel.app', 'https://vision-classroom2.onrender.com' , 'https://visionclassroo0m-jkiw.vercel.app'],
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
    credentials: true,
